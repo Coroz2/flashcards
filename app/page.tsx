b11c1c2c582d288525bdaa1b2d08ff7dbb3e2fe7
NekruzAsh <nekruzashrapov16@gmail.com>
2024-08-16 18:59:19 -0400

    updated landing page with a new design, installed daisyUI, added Tailwind configurations.

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import getStripe from "../utils/get-stripe"; // Adjust according to your project structure
+import "./globals.css";
 
 const HomePage: React.FC = () => {
   // Stripe checkout function
@@ -47,7 +48,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       {/* Header and Navigation */}
-      <AppBar position="static">
+      {/* <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             Flashcard SaaS
@@ -64,26 +65,78 @@ const HomePage: React.FC = () => {
             <UserButton />
           </SignedIn>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
+
+<div className="navbar bg-gradient-to-r from-indigo-600 via-purple-500 to-orange-400">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      
+    </div>
+    <a className="text-white btn btn-ghost text-xl">Flashcard SaaS</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a className="text-white">Resources</a></li>
+      <li>
+        <details>
+          <summary className="text-white">Products</summary>
+          <ul className="p-2">
+            <li><a className="text-white">Flashcards</a></li>
+            <li><a className="text-white">Research</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a className="text-white">Pricing</a></li>
+    </ul>
+  </div>
+  
+  <div className="navbar-end">
+  <SignedOut>
+            <Button className='butn1' color="inherit" href="/sign-in">
+              Sign in
+              <div className="arrow-wrapper">
+        <div className="arrow"></div>
+
+    </div>
+            </Button>
+          </SignedOut>
+  </div>
+</div>
+
+
+
 
       <Container>
         {/* Hero Section */}
-        <Box sx={{ textAlign: "center", my: 4 }}>
-          <Typography variant="h2" component="h1" gutterBottom>
-            Welcome to Flashcard SaaS
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            The easiest way to create flashcards from your text.
-          </Typography>
+        <Box className="w-full h-screen text-center relative">
+          <h1 className="mt-32 mx-32 ">Discover AI-Powered Effortless Learning.</h1>
+          <p className="mx-56 mt-6 text-[#5d6d7c]">
+  Unlock the power of AI with our cutting-edge Flashcard and PDF Scanner tool. Effortlessly transform your study materials and documents into interactive, customizable flashcards, and gain insights from PDFs like never before. Start learning smarter today!
+</p>
+
           <Button
             variant="contained"
-            color="primary"
+            color="secondary"
             sx={{ mt: 2, mr: 2 }}
             href="/generate"
           >
             Get Started
           </Button>
-          <Button variant="outlined" color="primary" sx={{ mt: 2 }}>
+          <Button variant="outlined" color="secondary" sx={{ mt: 2 }}>
             Learn More
           </Button>
         </Box>
